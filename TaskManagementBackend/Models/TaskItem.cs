e0f1a1b91ee7633cdf8420ce86385ecab1d3a1d8
HeiderDev <heiderhrnndz@gmail.com>
2025-03-15 11:07:25 -0500

    se crea el TaskItem

---
+++ TaskManagementBackend/Models/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagementBackend.Models
+{
+    public class TaskItem
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool? IsComplete { get; set; }
+
+    }
+}
