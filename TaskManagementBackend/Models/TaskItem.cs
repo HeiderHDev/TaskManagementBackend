0fb1fb65a4efbd6ca47c1119a0eb195abbc17a9f
HeiderDev <heiderhrnndz@gmail.com>
2025-03-15 16:55:21 -0500

    Se realizan valdiaciones en mi modelo de base de datos

--- TaskManagementBackend/Models/TaskItem.cs
+++ TaskManagementBackend/Models/TaskItem.cs
@@ -1,11 +1,29 @@
-﻿namespace TaskManagementBackend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementBackend.Models
 {
     public class TaskItem
     {
         public long Id { get; set; }
-        public string? Title { get; set; }
-        public string? Description { get; set; }
-        public bool? IsComplete { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
+        public required string Title { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(500, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
+        public required string Description { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public required bool IsComplete { get; set; }
+
+        // Campos de auditoría
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; } = null;
+
+        // Campo para soft delete
+        public bool IsDeleted { get; set; } = false;
 
     }
 }
